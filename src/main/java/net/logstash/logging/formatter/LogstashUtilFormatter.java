30279e2c16d0c904fdecbc00cd847584f01ac55f
Michał Kowalcze <Michal.Kowalcze@smart4aviation.aero>
2018-04-19 17:48:48 +0200

    Added the "thread" field.
    Added the "thread" field with name of the current thread to the list of
    logstash fields.

--- src/main/java/net/logstash/logging/formatter/LogstashUtilFormatter.java
+++ src/main/java/net/logstash/logging/formatter/LogstashUtilFormatter.java
@@ -97,11 +97,14 @@ public class LogstashUtilFormatter extends Formatter {
         builder.add("timestamp", record.getMillis());
         builder.add("level", record.getLevel().toString());
         builder.add("line_number", getLineNumber(record));
+        builder.add("thread", Thread.currentThread()
+                .getName());
         addSourceClassName(record, builder);
         addSourceMethodName(record, builder);
         addThrowableInfo(record, builder);
         return builder;
     }
+
 
     /**
      * Format the stackstrace.
